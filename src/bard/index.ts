f92492eafc96fd7b904e931a3c3db6fca3163d30
cc <y779102260@gmail.com>
2023-11-18 21:38:12 +0800

    test

--- src/bard/index.ts
+++ src/bard/index.ts
@@ -10,6 +10,7 @@ export async function ask(prompt: string, secure1psid = SECURE_1PSID) {
   }
 
   const cookies = `__Secure-1PSID=${secure1psid}`
+  console.log(cookies)
   const bot = new Bard(cookies)
 
   // 使用lodash生成随机字符串充当会话id (如果需要记忆会话，需要携带id)
