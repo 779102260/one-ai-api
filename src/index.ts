77fc55d4cf394958a42e2590dea17dbdf94a0d4f
cc <y779102260@gmail.com>
2023-11-23 20:48:52 +0800

    支持排序

--- src/index.ts
+++ src/index.ts
@@ -2,47 +2,57 @@ import * as OpenAI from './openai'
 import * as Azure from './azure'
 import * as Claude from './claude'
 import * as Bard from './bard'
+import { error } from 'console'
 
 export type IConfig = {
+  order?: ('openai' | 'claude' | 'bard' | 'azure')[]
   openai?: Partial<OpenAI.IAskConfig>
   azure?: Partial<Azure.IAskConfig>
   claude?: Partial<Claude.IAskConfig>
   bard?: Partial<Bard.IAskConfig>
+  /** 开启错误日志 */
+  degguer?: boolean
 }
 
 export async function askAuto(prompt: string, config: IConfig) {
-  let answer = ''
-  let ai: keyof IConfig | '' = ''
-  if (config.openai) {
-    ai = 'openai'
-    answer = await OpenAI.ask(prompt, config.openai.apiKey).catch((e) => {
-      console.error(`OpenAI error: ${e?.message}`, e)
-      return ''
-    })
+  const { order = ['openai', 'claude', 'bard', 'azure'], degguer } = config
+  for (const name of order) {
+    if (name === 'openai' && config.openai) {
+      const answer = await OpenAI.ask(prompt, config.openai.apiKey).catch((error) => {
+        degguer && console.error(error)
+        return ''
+      })
+      if (answer) {
+        return { answer, ai: name }
+      }
+    }
+    if (name === 'azure' && config.azure) {
+      const answer = await Azure.ask(prompt, config.azure.apiKey, config.azure.config).catch((error) => {
+        degguer && console.error(error)
+        return ''
+      })
+      if (answer) {
+        return { answer, ai: name }
+      }
+    }
+    if (name === 'claude' && config.claude) {
+      const answer = await Claude.ask(prompt, config.claude.orgId, config.claude.sessionKey).catch((error) => {
+        degguer && console.error(error)
+        return ''
+      })
+      if (answer) {
+        return { answer, ai: name }
+      }
+    }
+    if (name === 'bard' && config.bard) {
+      const answer = await Bard.ask(prompt, config.bard.secure1psid).catch((error) => {
+        degguer && console.error(error)
+        return ''
+      })
+      if (answer) {
+        return { answer, ai: name }
+      }
+    }
   }
-  if (!answer && config.azure) {
-    ai = 'azure'
-    answer = await Azure.ask(prompt, config.azure.apiKey, config.azure.config).catch((e) => {
-      console.error(`Azure error: ${e?.message}`, e)
-      return ''
-    })
-  }
-  if (!answer && config.claude) {
-    ai = 'claude'
-    answer = await Claude.ask(prompt, config.claude.orgId, config.claude.sessionKey).catch((e) => {
-      console.error(`Claude error: ${e?.message}`, e)
-      return ''
-    })
-  }
-  if (!answer && config.bard) {
-    ai = 'bard'
-    answer = await Bard.ask(prompt, config.bard.secure1psid).catch((e) => {
-      console.error(`Bard error: ${e?.message}`, e)
-      return ''
-    })
-  }
-  return {
-    answer,
-    ai,
-  }
+  return Promise.reject('全部失败')
 }
